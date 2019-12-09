cdf7a6268e53bc11493e08d9b463c9cda94b6476
Kadir <abdulkadir.erucu@telekom.de>
2019-12-09 16:18:19 +0100

    Spring 02: On the road

---
+++ src/test/java/com/wildcodeschool/wizardSite/WizardSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.wizardSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WizardSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
